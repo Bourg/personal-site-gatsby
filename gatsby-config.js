d989a979842e07085cecaab1deba92943efa8ea7
Austin Bourgerie <bourgerie.a@gmail.com>
2020-03-29 17:17:40 -0400

    Remove unused Gatsby files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Austin's Homepage`,
+    description: `Austin's work-in-progress personal homepage`,
+    author: `@bourg`,
   },
   plugins: [
+    `gatsby-plugin-typescript`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
