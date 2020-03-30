210b30eb3b1b65b1f8b8f3341409d781d3bb0fdc
Austin Bourgerie <bourgerie.a@gmail.com>
2020-03-29 21:48:14 -0400

    Remove dark mode code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,8 @@
 import React from "react"
-import Layout from "../components/layout"
 
 export default () => (
-  <Layout>
+  <div>
     <h1>My Personal Site</h1>
     <h2>Under construction!</h2>
-  </Layout>
+  </div>
 )
